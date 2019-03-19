cfb47b0b1c79f7c7e5981c5adf84e29ba20f3d63
Hannah Coffman <hcoffman7@gmail.com>
2019-03-19 00:35:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,16 @@ const driver = {}
 function updateDriverWithKeyAndValue(driver,key,value) {
   let driverCopy = {...driver}
   driverCopy[key] = value
+  return driverCopy
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value) {
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let driverCopy = {...driver}
+  delete driverCopy[key]
+  return driverCopy
 }
